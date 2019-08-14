d94f12d496168fa9916607acac8a11d6714c633d
dl <25194099+dclzs@users.noreply.github.com>
2019-08-14 22:05:36 +0800

    init project

---
+++ ao_common/src/main/java/entity/ResultEnum.java
@@ -0,0 +1,30 @@
+package entity;
+
+public enum ResultEnum {
+    SUCCESS(1024,"成功"),
+    FAILED(1023,"失败");
+
+    private int code;
+    private String msg;
+
+    ResultEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
